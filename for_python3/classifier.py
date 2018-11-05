bbdf39f6afb426170571b05ca43c255418958bb9
loominem <37760868+loominem@users.noreply.github.com>
2018-11-05 17:38:58 +0800

    for py 3

---
+++ for_python3/classifier.py
@@ -0,0 +1,129 @@
+import sys
+import csv
+import math
+from collections import Counter
+
+from feature import (
+    TitleFeature,
+    PublisherFeature,
+    HostnameFeature,
+)
+
+
+class NewsClassifier(object):
+    """
+    News Article Classifier to classify new article.
+    """
+
+    def __init__(self):
+        self.name = 'new artical classifier'
+        self.features = {}
+        self.categories = {}
+
+    def learn(self, file_path):
+        """
+        Learn traning data give the training data path.
+        """
+        # Create two features's feature class, namely news's title and news's publisher
+        # training data, each data record is a list of article_id, title, url, publisher, hostname, timestamp, category.
+        training_data = self.read_csv(file_path)
+        self.features['title'] = TitleFeature(training_data[1:], smoothing_factor=0.01, word_joins=[1])
+        # self.features['publisher'] = PublisherFeature(training_data)
+        # self.features['hostname'] = HostnameFeature(training_data[1:], smoothing_factor=1.0)
+
+        self.categories = Counter([record[6] for record in training_data[1:]])
+
+        # Write predict of training data to see the difference
+        training_pred = self.predict_dataset(training_data[:], print_ids=[])
+        compare = [['article_id', 'category', 'pred']]
+        diff_count = 0
+        for train, pred in zip(training_data[1:], training_pred):
+            if train[6] != pred[1]:
+                diff_count += 1
+            compare.append([train[0], train[6], pred[1]])
+        print ('Total number of difference is', diff_count, 'out of', len(training_pred), 'records')
+        self.write_csv('./data/diff.csv', compare)
+
+    # for cat in self.categories:
+    #     print 'current cat ', cat + ' having record ', self.categories[cat]
+
+    def predict(self, test_record, print_ids=None):
+        """
+        Predict the category against the given testing news record using Naive Baysian
+        :param test_record:
+        :param print_ids: the article records id that needs printing
+        :return: the category label
+        """
+        # For each possible category, compare the log probability
+        need_print = test_record[0] in print_ids if print_ids else False
+        max_log_prob = None
+        result = None
+        for cat in self.categories:
+            log_prob = 0
+            # Iterates each features
+            for feature in self.features.values():
+                log_prob += feature.condition_log_prob(test_record, cat, print_ids=print_ids)
+                if need_print:
+                    print ('[classifier] feature=', feature.name, 'log_prob=', log_prob)
+            # Adding the prior
+            log_prob += math.log(self.categories[cat] * 1.0 / sum(self.categories.values()))
+            if need_print:
+                print ('[classifier] current cat=', cat, 'log_prob with prior=', log_prob, "\n")
+            if max_log_prob is None or max_log_prob < log_prob:
+                max_log_prob = log_prob
+                result = cat
+        if need_print:
+            print ('[classifier] Article', test_record[0], 'Final max_log=', max_log_prob, 'with category=', result)
+        return result
+
+    def predict_dataset(self, test_dataset=None, file_path=None, print_ids=[]):
+        """
+        A list of test data record or given test data file path in csv format
+        Each test data record in the form of [article_id, title, url, publisher, hostname, timestamp]
+        :param test_dataset: Should contain headers
+        :param file_path: csv file with headers
+        :param print_ids: the article records id that needs printing
+        :return: a list of predict tuples. tuple contains (article_id, category)
+        """
+        test_dataset = test_dataset or self.read_csv(file_path)
+        result = []
+        for test_record in test_dataset[1:]:
+            pred = self.predict(test_record, print_ids=print_ids)
+            result.append([test_record[0], pred])
+        return result
+
+    @classmethod
+    def read_csv(cls, file_path):
+        dataset = []
+        with open(file_path, 'r', encoding='utf-8') as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            # Omit the First line of titles
+            for row in csv_reader:
+                # Empty line
+                if not row:
+                    continue
+                dataset.append(row)
+        # Skip header
+        return dataset
+
+    @classmethod
+    def write_csv(cls, file_path, dataset):
+        with open(file_path, 'w', newline='') as f:
+            csv_writer = csv.writer(f, delimiter=',')
+            for row in dataset:
+                csv_writer.writerow(row)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print ('Please specify the training data file path')
+
+    news_classifier = NewsClassifier()
+    news_classifier.learn(sys.argv[1])
+
+    if len(sys.argv) == 3:
+        pred_result = news_classifier.predict_dataset(file_path=sys.argv[2])
+        pred_result = [('article_id', 'category')] + pred_result
+        test_file_split = sys.argv[2].split('.')
+        output_filepath = '.'.join(test_file_split[:-1]) + "_pred." + test_file_split[-1]
+        news_classifier.write_csv(output_filepath, pred_result)
---
+++ for_python3/feature.py
@@ -0,0 +1,160 @@
+import re
+import math
+
+
+class Feature(object):
+    """
+    Feature class to represent one feature of the data set given the feature index.
+    """
+
+    def __init__(self, name, feature_idx, smoothing_factor=1.0):
+        self.name = name
+        self.feature_idx = feature_idx
+        self.smoothing_factor = smoothing_factor
+
+    def condition_log_prob(self, test_record, category, print_ids=None):
+        """
+        Returning the log conditional probability of passed test_record of current feature given certain category.
+        :param test_record: news article record in the form of a list [article_id, title, url, publisher, hostname, timestamp]
+        :param category: the class
+        :param print_ids: the record ids that need to print
+        :return:
+        """
+        raise NotImplementedError()
+
+
+class TitleFeature(Feature):
+    """
+    Feature class representing the title attribute of the data records
+    """
+
+    def __init__(self, training_data, smoothing_factor=1.0, word_joins=None):
+        """
+        :param training_data: A list of training data records.
+        Each record is a list consisting of article_id, title, url, publisher, hostname, timestamp, category.
+        :param smoothing_factor:
+        :param word_joins: list. allowed number of words to join together to form a new word. e.g. sentence is 'what the fuck'
+        if word_joins is [1, 2], then formed vocabulary woulb be [what, the, fuck, what_the, the_fuck].
+        default to [1] is None is passed
+        """
+        super(TitleFeature, self).__init__('Title', 1, smoothing_factor)
+
+        self.word_joins = word_joins if word_joins else [1]
+        # Hold the word count for word in each category.
+        self.category_bag_of_words = {}
+        # Hold the total word count for each category.
+        self.category_count = {}
+        for record in training_data:
+            category = record[6]
+            if self.category_bag_of_words.get(category) is None:
+                self.category_bag_of_words[category] = {}
+                self.category_count[category] = 0
+            bag_of_words = self.category_bag_of_words[category]
+            for word in self._permutate_words(record[1].lower()):
+                if word not in bag_of_words:
+                    bag_of_words[word] = 0
+                    self.category_count[category] += 1 # dont count duplicate word in the same record
+                bag_of_words[word] += 1
+                #self.category_count[category] += 1
+        # for k, bw in self.category_bag_of_words.items():
+        #     print 'category ', k, ' with number of different words', len(bw)
+
+    def _permutate_words(self, sentence):
+        word_split = [w for w in re.split(r"\W", sentence.lower()) if w.strip()]
+        result = []
+        for i in self.word_joins:
+            joined_words = ['_'.join(word_split[j: j+i]) for j in range(len(word_split)-i + 1)]
+            result += joined_words
+        return result
+
+    def condition_log_prob(self, test_record, category, print_ids=None):
+        need_print = test_record[0] in print_ids if print_ids else False
+        feature_value = test_record[self.feature_idx]
+        if category not in self.category_count.keys():
+            raise AttributeError('Target category {} does not exist'.format(category))
+        log_prob = 0
+        for word in self._permutate_words(feature_value.lower()):
+            word_count = self.category_bag_of_words[category].get(word, 0) + self.smoothing_factor
+            total_word_count = (len(self.category_bag_of_words[category]) * self.smoothing_factor + self.category_count[category])
+            log_prob += math.log(word_count * 1.0 / total_word_count)
+            if need_print:
+                print ('[title] word=', word, 'word count=', word_count, 'total word count=', total_word_count, 'log_prob=', log_prob)
+        if need_print:
+            print ('[title] value=', feature_value, 'log_prob=', log_prob)
+        return log_prob
+
+
+class PublisherFeature(Feature):
+    """
+    Feature class representing the news publisher attribute of the data records
+    """
+
+    def __init__(self, training_data, smoothing_factor=1.0):
+        """
+        A list of training data records.
+        Each record is a list consisting of article_id, title, url, publisher, hostname, timestamp, category.
+        :param data:
+        """
+        super(PublisherFeature, self).__init__('Publisher', 3, smoothing_factor)
+        self.category_bag_of_publishers = {}
+        for record in training_data:
+            category = record[6]
+            if self.category_bag_of_publishers.get(category) is None:
+                self.category_bag_of_publishers[category] = {}
+            publishers = self.category_bag_of_publishers[category]
+            publisher_name = record[3].strip().lower()
+            if publisher_name not in publishers:
+                publishers[publisher_name] = 0
+            publishers[publisher_name] += 1
+        # for k, bw in self.category_bag_of_publishers.items():
+        #     print 'category ', k, ' with number of different publisher', len(bw)
+
+    def condition_log_prob(self, test_record, category, print_ids=None):
+        need_print = test_record[0] in print_ids if print_ids else False
+        feature_value = test_record[self.feature_idx]
+        if category not in self.category_bag_of_publishers:
+            raise AttributeError('Target category {} does not exist'.format(category))
+        category_hostname_count = self.category_bag_of_publishers[category].get(feature_value.strip().lower(), 0) + self.smoothing_factor
+        category_total_count = sum(self.category_bag_of_publishers[category].values()) + len(self.category_bag_of_publishers[category]) * self.smoothing_factor
+        log_prob = math.log(category_hostname_count * 1.0 / category_total_count)
+        if need_print:
+            print ('[publisher] value=', feature_value, 'count=', category_hostname_count, 'total=', category_total_count, 'log_prob=', log_prob)
+        return log_prob
+
+
+class HostnameFeature(Feature):
+    """
+    Feature class representing the news hostname attribute of the data records
+    """
+
+    def __init__(self, training_data, smoothing_factor=1.0):
+        """
+        A list of training data records.
+        Each record is a list consisting of article_id, title, url, publisher, hostname, timestamp, category.
+        :param data:
+        """
+        super(HostnameFeature, self).__init__('Hostname', 4, smoothing_factor)
+        self.category_bag_of_hostname = {}
+        for record in training_data:
+            category = record[6]
+            if self.category_bag_of_hostname.get(category) is None:
+                self.category_bag_of_hostname[category] = {}
+            publishers = self.category_bag_of_hostname[category]
+            publisher_name = record[4].strip().lower()
+            if publisher_name not in publishers:
+                publishers[publisher_name] = 0
+            publishers[publisher_name] += 1
+        # for k, bw in self.category_bag_of_hostname.items():
+        #     print 'category ', k, ' with number of different hostname', len(bw)
+
+    def condition_log_prob(self, test_record, category, print_ids=None):
+        need_print = test_record[0] in print_ids if print_ids else False
+        feature_value = test_record[self.feature_idx]
+        if category not in self.category_bag_of_hostname:
+            raise AttributeError('Target category {} does not exist'.format(category))
+        category_hostname_count = self.category_bag_of_hostname[category].get(feature_value.strip().lower(), 0) + self.smoothing_factor
+        category_total_count = sum(self.category_bag_of_hostname[category].values()) + self.smoothing_factor * len(self.category_bag_of_hostname[category])
+        log_prob = math.log(category_hostname_count * 1.0 / category_total_count)
+        if need_print:
+            print ('[hostname] value=', feature_value, 'count=', category_hostname_count, 'total=', category_total_count, 'log_prob=', log_prob)
+        return log_prob
